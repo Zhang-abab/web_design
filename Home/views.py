3563905533d8cbfd242a2dc45713cb993a937f7f
Zhang_abab <2411358676@qq.com>
2022-02-15 15:55:01 +0800

    Heroku
    测试集成部署

--- Home/views.py
+++ Home/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the  index.")
+    return HttpResponse("Hello, world. You're at the  1233 index.")
 
