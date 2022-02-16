957feac9aaf5b0cdd37ff148e73131331368855b
Zhang_abab <2411358676@qq.com>
2022-02-16 15:04:07 +0800

    更正

--- Introduce/views.py
+++ Introduce/views.py
@@ -1,34 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< f4fc776c58b415edc252e933b892089ccaaede05
-=======
->>>>>>> master
-from turtle import ht
+
 from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
-<<<<<<< HEAD
-=======
-from django.shortcuts import render
->>>>>>> Introduce
-=======
->>>>>>> master
 
 
 # Create your views here.
 def editor(request):
-<<<<<<< HEAD
-<<<<<<< f4fc776c58b415edc252e933b892089ccaaede05
-=======
->>>>>>> master
     return render(request, 'HTML/editor.html')
 
 def index(request):
     return render(request, 'HTML/I_index.html')
-<<<<<<< HEAD
-=======
-    return render(request, 'editor.html')
->>>>>>> Introduce
-=======
->>>>>>> master
 
 
