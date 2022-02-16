92cf573ac6c23d0647b9b71a3888f95c1012c4f3
Zhang_abab <2411358676@qq.com>
2022-02-16 14:43:03 +0800

    Introfuce
    editor 页面

--- Home/urls.py
+++ Home/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+<<<<<<< f4fc776c58b415edc252e933b892089ccaaede05:Home/urls.py
     path('', views.index, name='index'),
 ]
+=======
+    path('editor/', views.editor, name='editor'), #编辑器测试
+    path('index/', views.index, name='index')
+]
+>>>>>>> Introfuce:Introduce/urls.py
---
+++ Introduce/views.py
@@ -0,0 +1,14 @@
+from turtle import ht
+from urllib import request
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def editor(request):
+    return render(request, 'HTML/editor.html')
+
+def index(request):
+    return render(request, 'HTML/I_index.html')
+
+
