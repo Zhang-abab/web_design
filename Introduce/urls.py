c5db558aab36e686ac4617f9d6a6843f2b9fcd3c
Zhang_abab <2411358676@qq.com>
2022-03-01 15:38:18 +0800

    format
    format_file

--- Introduce/urls.py
+++ Introduce/urls.py
@@ -8,12 +8,21 @@ app_name = 'Introduce'
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('editor/', views.editor, name='editor'), #编辑器测试
+    path('editor/<str:content>',views.editor_content),
     path('entity/', views.entity, name='entity'), #实体
+    path('entity/<str:content>',views.entity_content),
     path('mate/', views.mate, name='mate'),#mate标签
+    path('mate/<str:content>',views.mate_content),
     path('semantic/', views.semantic, name='semantic'),#语义化标签
+    path('semantic/<str:content>',views.semantic_content),
     path('list/', views.list, name='list'),#列表
-    path('links/', views.links, name='links'),#超链接
-    path('picture/',views.pciture, name='picture'),#图片标签
-    path('inline/', views.inline, name='inline'),#内联标签
-    path('multi/', views.multi, name='multi'),#媒体    
+    path('list/<str:content>',views.list_content),
+    # path('links/', views.links, name='links'),#超链接
+    # path('links/<str:content>',views.links_content),
+    # path('picture/',views.pciture, name='picture'),#图片标签
+    # path('picture/<str:content>',views.picture_content),
+    # path('inline/', views.inline, name='inline'),#内联标签
+    # path('inline/<str:content>',views.inline_content),
+    # path('multi/', views.multi, name='multi'),#媒体  
+    # path('multi/<str:content>',views.multi_content),  
 ]
--- Introduce/views.py
+++ Introduce/views.py
@@ -6,22 +6,63 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'I_index.html')
+    return render(request, 'introduce/index.html')
 
 def editor(request):
-    return render(request, 'editor.html')
+    return render(request, 'introduce/editor/editor.html')
 
+def editor_content(request,content):
+    if content == 'examples':
+        return render(request,'introduce/editor/examples.html')
+    elif content == 'practice':
+        return render(request,'introduce/editor/practice.html')
+    else:
+        return HttpResponse('---非法请求---')
+        
 def entity(request):
-    return render(request, 'entity.html')
+    return render(request, 'introduce/entity/entity.html')
+
+def entity_content(request,content):
+    if content == 'examples':
+        return render(request,'introduce/entity/examples.html')
+    elif content == 'practice':
+        return render(request,'introduce/entity/practice.html')
+    else:
+        return HttpResponse('---非法请求---')
+
 
 def mate(request):
-    return render(request, 'mate.html')
+    return render(request, 'introduce/mate/mate.html')
+
+def mate_content(request,content):
+    if content == 'examples':
+        return render(request,'introduce/mate/examples.html')
+    elif content == 'practice':
+        return render(request,'introduce/mate/practice.html')
+    else:
+        return HttpResponse('---非法请求---')
 
 def semantic(request):
-    return render(request, 'semantic.html')
+    return render(request, 'introduce/semantic/semantic.html')
+
+def semantic_content(request,content):
+    if content == 'examples':
+        return render(request,'introduce/semantic/examples.html')
+    elif content == 'practice':
+        return render(request,'introduce/semantic/practice.html')
+    else:
+        return HttpResponse('---非法请求---')
 
 def list(request):
-    return render(request, "list.html")
+    return render(request, "introduce/list/list.html")
+
+def list_content(request,content):
+    if content == 'examples':
+        return render(request,'introduce/list/examples.html')
+    elif content == 'practice':
+        return render(request,'introduce/list/practice.html')
+    else:
+        return HttpResponse('---非法请求---')
 
 def links(request):
     return render(request, "links.html")
@@ -34,3 +75,6 @@ def inline(request):
 
 def multi(request):
     return render(request, "multi.html")
+
+def editor_Examples(request):
+    return render(request,'editor_Examples.html')
